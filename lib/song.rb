de0ef8e266dfda2b0f9b68630d34e4625e51c6d2
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-10-02 02:14:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 class Song 
   attr_accessor :artist,:name,:genre
-  def initialize (artist,name,genre)
+ 
+  def initialize (name,artist,genre)
   
     @name = name
-  
+  @artist = artist
+  @genre =g enre
 end
 end
