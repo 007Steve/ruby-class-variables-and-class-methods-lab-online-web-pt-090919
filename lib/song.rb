854cd1d71a61fbd47b667840f30cfb79806dd19d
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-10-02 02:32:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,6 @@ class Song
     end
     def self. genre_count 
       genre_count = hash.new(0)
-      @@genre_count.each do |genre| genre_count[genre] += 1 end
+      @@genre_count.each {|genre| genre_count[genre] += 1}
       genre_count
   end
